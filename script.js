f2bba6dc68cbbdd3d0d18b7d6ad46b5645cf6e08
Rocio7531 <rocio.yut@gmail.com>
2026-04-30 18:55:11 +0200

    New changes

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const API_URL = "https://telco-churn-api-cj8e.onrender.com/predict";
 
-document.getElementById("churn-form").addEventListener("submit", async function (event) {
-    event.preventDefault();
 document.getElementById("clear-btn").addEventListener("click", function () {
     document.getElementById("churn-form").reset();
     document.getElementById("result").innerHTML = "";
 });
+//Form Submit
+document.getElementById("churn-form").addEventListener("submit", async function (event) {
+    event.preventDefault();
 
     const payload = {
         gender: document.getElementById("gender").value,
